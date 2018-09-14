d4e6a1b99dfa945c338a0ec93dcd71d48224af70
Ivan Kyuchukov <vaka.stz@gmail.com>
2018-09-14 08:12:51 +0300

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="compFooter">&copy; Vako - 2018</footer>
+    );
+  }
+}
+
+export default Footer;
